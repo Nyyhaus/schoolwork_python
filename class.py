a335e7930673a0bb74cb175ea7abf664f00632d7
Ville Uusitalo <75932298+Nyyhaus@users.noreply.github.com>
2021-02-17 12:54:11 +0200

    Create class.py

---
+++ class.py
@@ -0,0 +1,10 @@
+class Kilpailija:
+	"""Luokka määrittelee pisteet ja värin"""
+	pisteet = ""
+	vari = ""
+	
+eka = Kilpailija()
+eka.pisteet = 10
+eka.vari = "sininen"
+
+print(f"Kilpailijalla {eka.vari} on {eka.pisteet} pistettä!")
