4f136be6765cf3e072affab16e781d8e6b361653
Ville Uusitalo <75932298+Nyyhaus@users.noreply.github.com>
2021-02-17 12:55:30 +0200

    Create array1.js

---
+++ array1.js
@@ -0,0 +1,7 @@
+lista = ["Sininen", "Punainen", "Keltainen", "Vihreä"]
+eka = lista[0]
+print("Listan ensimmäinen alkio on: ", eka)
+
+print("Lista tulostettuna alkio kerrallaan:")
+for i in lista:
+	print(i)
